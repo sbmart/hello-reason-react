585af38e98f7fe27a6f3478b7b8c4257cbb47b3d
sbmart <sbmart73@gmail.com>
2019-02-11 12:08:42 +0300

    adding a component

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -3,11 +3,14 @@
 
 var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
+var Greeting$ReactTemplate = require("./Greeting.bs.js");
 var Component1$ReactTemplate = require("./Component1.bs.js");
 var Component2$ReactTemplate = require("./Component2.bs.js");
 
 ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, Component1$ReactTemplate.make("Hello! Click this text.", /* array */[])), "index1");
 
 ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, Component2$ReactTemplate.make("Hello!", /* array */[])), "index2");
+
+ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, Greeting$ReactTemplate.make("John", /* array */[])), "greeting");
 
 /*  Not a pure module */
